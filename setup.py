27a8bd05b9243d3c2998437706da58f2d0d6bf54
ALERT <alexey.rubasheff@gmail.com>
2020-06-27 11:59:12 +0300

    -py27

--- setup.py
+++ setup.py
@@ -42,14 +42,14 @@ test_requirements = requirements
 setup(
     author="Alexey Rubasheff",
     author_email='alexey.rubasheff@gmail.com',
-    python_requires='>=2.7',
+    python_requires='3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.5',
         # 'Programming Language :: Python :: 3.6',
@@ -69,6 +69,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
