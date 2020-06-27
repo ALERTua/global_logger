4a2a703a2772aa3af152dbe633168958afe8fe2e
ALERT <alexey.rubasheff@gmail.com>
2020-06-27 16:37:00 +0300

    v0.2.4

--- global_logger/__init__.py
+++ global_logger/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Alexey Rubasheff"""
 __email__ = 'alexey.rubasheff@gmail.com'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from .global_logger import Log   # noqa # pylint: disable=unused-import
--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
