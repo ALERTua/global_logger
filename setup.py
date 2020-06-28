7bf9ea2b0c361b53f9c85b2f277cf11cccdb8dbd
ALERT <alexey.rubasheff@gmail.com>
2020-06-28 16:25:49 +0300

    ! bump v0.2.11

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.2.10',
+    version='0.2.11',
     zip_safe=False,
 )
