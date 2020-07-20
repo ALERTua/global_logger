a055cc008b29f971a02a1f19087ca741dac71e53
ALERT <alexey.rubasheff@gmail.com>
2020-07-20 17:22:02 +0300

    v0.3.9

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.3.8',
+    version='0.3.9',
     zip_safe=False,
 )
