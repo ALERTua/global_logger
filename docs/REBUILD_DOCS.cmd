5d6330ef818e94eed4fa7fc6894da30962066445
ALERT <alexey.rubasheff@gmail.com>
2020-07-20 15:04:25 +0300

    ! docs exclude

--- docs/REBUILD_DOCS.cmd
+++ docs/REBUILD_DOCS.cmd
@@ -4,6 +4,6 @@ call %~dp0constants.cmd
 
 call %VENV_PIP% install sphinx sphinx-rtd-theme
 call %~dp0_sphinx-apidoc.cmd
-call %~dp0_make.bat html
+call %~dp0_make.bat html || pause & exit /b
 start "" "%~dp0build\\html\\index.html"
 exit /b
