6056c8e5967d9cf214a251f995754bc4da91df32
Alexey Rubasheff <alexey.rubasheff@gmail.com>
2021-08-17 14:39:30 +0300

    ! v0.3.22

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.3.21',
+    version='0.3.22',
     zip_safe=False,
 )
