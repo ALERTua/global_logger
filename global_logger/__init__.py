3abcc30a8320857a60daaa00e1637e2edeb9f659
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2021-10-15 11:09:44 +0300

    ! v0.3.24

--- global_logger/__init__.py
+++ global_logger/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Alexey Rubasheff"""
 __email__ = 'alexey.rubasheff@gmail.com'
-__version__ = '0.3.23'
+__version__ = '0.3.24'
 
 from .global_logger import Log   # noqa # pylint: disable=unused-import
