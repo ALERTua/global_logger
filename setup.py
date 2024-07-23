439bee7459607b1d4062f718ef9b74d3cc22f168
Aleksei Rubashev <a_rubashev@wargaming.net>
2024-07-23 18:16:01 +0300

    ! v0.4.0

--- setup.py
+++ setup.py
@@ -71,6 +71,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/alertua/global_logger',
-    version='0.3.4',
+    version='0.4.0',
     zip_safe=False,
 )
